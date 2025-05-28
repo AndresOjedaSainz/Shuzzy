5cfa784d37f0974ef7eef488836036749888149e
John Kilroy <55547547+John-Kilroy@users.noreply.github.com>
2025-05-27 21:35:41 -0400

    Update code.js

--- js/code.js
+++ js/code.js
@@ -14,11 +14,15 @@ function doLogin()
 	let login = document.getElementById("loginName").value;
 	let password = document.getElementById("loginPassword").value;
 	var hash = md5( password );
+
+	if (!validLoginForm(login, password)) {
+        document.getElementById("loginResult").innerHTML = "invalid username or password";
+        return;
+    	}
 	
 	document.getElementById("loginResult").innerHTML = "";
 
-	let tmp = {login:login,password:password};
-//	var tmp = {login:login,password:hash};
+	let tmp = {login:login,password:hash};
 	let jsonPayload = JSON.stringify( tmp );
 	
 	let url = urlBase + '/Login.' + extension;
