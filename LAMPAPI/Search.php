acf637002b5fa751e6cda6f1b2a02ea241d020b7
Noah Fourari <155576891+nfourari@users.noreply.github.com>
2025-06-03 12:42:53 -0400

    Update Search.php

--- LAMPAPI/Search.php
+++ LAMPAPI/Search.php
@@ -1,4 +1,18 @@
 <?php
+
+
+    // CORS headers for permissions
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
+
+    // Handle preflight OPTIONS request
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        http_response_code(200);
+        exit();
+    }
+
+    
     $inData = getRequestInfo();
     
     $searchResults = "";
