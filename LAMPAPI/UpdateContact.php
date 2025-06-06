bc16ac842749a35a7d14005abe540f5bb71b3b4d
AndresOjedaSainz <aojedasainz30@gmail.com>
2025-06-06 18:57:04 -0400

    Update UpdateContact.php

--- LAMPAPI/UpdateContact.php
+++ LAMPAPI/UpdateContact.php
@@ -19,7 +19,7 @@ $lastName = $inData["lastName"];
 $phoneNumber = $inData["phoneNumber"];
 $emailAddress = $inData["emailAddress"];
 
-$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+$conn = new mysqli("localhost", "poostsqlacc", "POOSTApp2025!", "COP4331");
 
 if ($conn->connect_error) {
     returnWithError($conn->connect_error);
