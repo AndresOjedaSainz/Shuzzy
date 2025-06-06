117ac7b00fff264318d4ec18c0bb357571199f11
AndresOjedaSainz <aojedasainz30@gmail.com>
2025-06-06 18:56:47 -0400

    Update SearchContact.php

--- LAMPAPI/SearchContact.php
+++ LAMPAPI/SearchContact.php
@@ -16,7 +16,7 @@ $inData = getRequestInfo();
 $searchResults = "";
 $searchCount = 0;
 
-$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+$conn = new mysqli("localhost", "poostsqlacc", "POOSTApp2025!", "COP4331");
 if ($conn->connect_error) {
     returnWithError($conn->connect_error);
 } else {
